8fd5a831f58ad5a945217f01c20b04b908fd4e09
heayoon <heayoon381@gmail.com>
2024-05-27 10:56:00 +0900

    Feat: cat,cd 헤더파일 수정

--- includes/cat_.h
+++ includes/cat_.h
@@ -4,11 +4,8 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <sys/fcntl.h>
+# include <string.h>
 # include <fcntl.h>
-# include <errno.h>
-# include <sys/types.h>
-# include <sys/stat.h>
 # include <sys/syscall.h>
 # include <signal.h>
 
--- includes/cd_.h
+++ includes/cd_.h
@@ -4,11 +4,9 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <sys/fcntl.h>
+# include <string.h>
 # include <fcntl.h>
 # include <errno.h>
-# include <sys/types.h>
-# include <sys/stat.h>
 # include <sys/syscall.h>
 
 
